1c0f179c44174a9b543dc56aef77c3a50199d5e6
Munak <oiool@naver.com>
2018-01-03 17:10:19 +0900

    Initial Commit

---
+++ LivingMockUp3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LivingMockUp3
+//
+//  Created by WinterSky on 2018. 1. 3..
+//  Copyright © 2018년 WinterSky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
